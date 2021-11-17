7558a8719b8e64741ac7d7f44b40417319f6c3db
Massimiliano <utzerimassimiliano@gmail.com>
2021-11-17 14:32:23 +0100

    Starter libreria primerCommit (no compila)

---
+++ src/main/java/starter/SaludoAutoconfigure.java
@@ -0,0 +1,29 @@
+package starter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(Saludo.class)
+@EnableConfigurationProperties(SaludoProperties.class)
+public class SaludoAutoconfigure {
+	
+	private final SaludoProperties properties;
+	
+	public SaludoAutoconfigure(SaludoProperties properties) {
+		this.properties = properties;
+	}
+	
+	@Bean
+	public Saludo saludo() {
+		return new Saludo(properties.getIdioma());
+	}
+
+}
